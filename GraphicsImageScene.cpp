6bd73ef8727655ac755e1b451bbc9099b68f0280
clem <clem.delambily@gmail.com>
2016-01-04 11:52:19 +0100

    modif inpainting

--- GraphicsImageScene.cpp
+++ GraphicsImageScene.cpp
@@ -1,13 +1,14 @@
 #include "GraphicsImageScene.h"
+#include <QDebug>
 
 GraphicsImageScene::GraphicsImageScene(QObject * parent):
-  QGraphicsScene(parent),
+  DrawableGraphicsScene(parent),
   m_pixmapItem(new QGraphicsPixmapItem)
 {
   QGraphicsScene::addItem(m_pixmapItem);
 }
 
-void GraphicsImageScene::setImage(QImage const& img)
+/*void GraphicsImageScene::setImage(QImage const& img)
 {
   m_img = img;
   m_pixmapItem->setPixmap(QPixmap::fromImage(m_img));
@@ -16,4 +17,32 @@ void GraphicsImageScene::setImage(QImage const& img)
 QImage GraphicsImageScene::image() const
 {
   return m_img;
+}
+*/
+
+void GraphicsImageScene::mousePressEvent(QMouseEvent *e)
+{
+    setInteractive(true);
+    setBrushVisibility(true);
+
+    mask();
+    //redrawBrush(e->localPos());
+    if ( isInteractive() == true){
+
+        if ( e->button() == Qt::LeftButton ){
+            paint(e->localPos());
+            qDebug() << "clic gauche";
+        }
+        else if( e->button() == Qt::RightButton ){
+            erase(e->localPos());
+        }
+
+        //redrawBrush(e->localPos());
+    }
+}
+
+void GraphicsImageScene::mouseReleaseEvent(QMouseEvent *e)
+{
+    if ( e->button() )
+        setBrushVisibility(false);
 }
